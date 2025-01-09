f48a543eb3eb2132314b9cae39054e9d42072084
jgelia <jgelia@ncsu.edu>
2025-01-08 20:22:55 -0500

    Add comments to thread manager

--- src/main/java/frc/robot/subsystems/vision/ThreadManager.java
+++ src/main/java/frc/robot/subsystems/vision/ThreadManager.java
@@ -8,15 +8,33 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Manages threads for various tasks within the robot's vision system.
+ * Provides methods to start, stop, and manage threads efficiently while preventing excessive updates.
+ * 
+ * @author Ishan
+ */
 public class ThreadManager {
+
   private final ExecutorService executorService;
   private final Map<String, Future<?>> threadFutures = new ConcurrentHashMap<>();
 
+  /**
+   * Creates a ThreadManager with a fixed number of threads.
+   *
+   * @param maxThreads the maximum number of threads that can run simultaneously.
+   */
   public ThreadManager(int maxThreads) {
     executorService = Executors.newFixedThreadPool(maxThreads);
   }
 
-  // Start a thread for a specific task
+  /**
+   * Starts a new thread to run a specific task. If a thread with the given name is already running,
+   * the method will print an error message and do nothing.
+   *
+   * @param threadName the name of the thread to start.
+   * @param task the task to run on the thread, this can be a function.
+   */
   public void startThread(String threadName, Runnable task) {
     if (threadFutures.containsKey(threadName)) {
       System.err.println("Thread " + threadName + " is already running.");
@@ -36,7 +54,11 @@ public class ThreadManager {
     threadFutures.put(threadName, future);
   }
 
-  // Stop a specific thread
+  /**
+   * Stops a specific thread by its name. If the thread is not running, an error message is printed.
+   *
+   * @param threadName the name of the thread to stop.
+   */
   public void stopThread(String threadName) {
     Future<?> future = threadFutures.get(threadName);
     if (future != null) {
@@ -47,7 +69,14 @@ public class ThreadManager {
     }
   }
 
-  // Submit a vision input task
+  /**
+   * Starts a vision-related task that periodically updates vision inputs.
+   * This method creates a thread that repeatedly executes the provided task until interrupted.
+   *
+   * @param threadName the name of the thread to start.
+   * @param inputs the vision inputs to be updated.
+   * @param visionTask the task that updates vision inputs.
+   */
   public void startVisionInputTask(String threadName, VisionInputs inputs, Runnable visionTask) {
     startThread(
         threadName,
@@ -63,7 +92,10 @@ public class ThreadManager {
         });
   }
 
-  // Shut down all threads
+  /**
+   * Shuts down all threads managed by this instance. Attempts a graceful shutdown, then forces it
+   * if necessary.
+   */
   public void shutdownAllThreads() {
     threadFutures.values().forEach(future -> future.cancel(true));
     threadFutures.clear();
--- src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
@@ -6,8 +6,17 @@ import frc.robot.subsystems.vision.VisionConstants.Limelight;
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 
+/**
+ * VisionSubsystem is a subsystem for managing vision-related tasks in the robot, including
+ * interacting with limelights, processing vision data, and logging inputs.
+ * 
+ * It provides methods for getting detailed vision information, setting heading
+ * data for pose estimation, and retrieving poses based on April Tag detection.
+ * 
+ * @author Ishan
+ */
 public class VisionSubsystem extends SubsystemBase {
-  
+
   private final VisionInterface visionInterface;
   private final VisionInputsAutoLogged inputs = new VisionInputsAutoLogged();
 
@@ -23,36 +32,83 @@ public class VisionSubsystem extends SubsystemBase {
     Logger.processInputs("Vision/", inputs);
   }
 
-  // Add methods to support DriveCommand
+  /**
+   * Gets the number of April Tags detected by a specified Limelight.
+   *
+   * @param limelight a limelight (BACK, FRONT_LEFT, FRONT_RIGHT).
+   * @return The number of April Tags detected by the specified Limelight.
+   */
   public int getNumberOfAprilTags(Limelight limelight) {
     return inputs.limelightTargets[limelight.getId()];
   }
 
+  /**
+   * Gets the distance to the April Tags detected by a specified Limelight.
+   *
+   * @param limelight a limelight (BACK, FRONT_LEFT, FRONT_RIGHT).
+   * @return The distance to the April Tags detected, in meters.
+   */
   public double getLimelightAprilTagDistance(Limelight limelight) {
     return inputs.limelightAprilTagDistances[limelight.getId()];
   }
 
+  /**
+   * Gets the timestamp of the last data received from a specified Limelight.
+   *
+   * @param limelight a limelight (BACK, FRONT_LEFT, FRONT_RIGHT).
+   * @return The timestamp of the last data in seconds.
+   */
   public double getTimeStampSeconds(Limelight limelight) {
     return inputs.limelightTimestamps[limelight.getId()];
   }
 
+  /**
+   * Gets the latency of a specified Limelight in seconds.
+   *
+   * @param limelight a limelight (BACK, FRONT_LEFT, FRONT_RIGHT).
+   * @return The latency of the Limelight in seconds.
+   */
   public double getLatencySeconds(Limelight limelight) {
     return inputs.limelightLatencies[limelight.getId()];
   }
 
+  /**
+   * Sets heading information for the robot. This information is used to assist in pose
+   * estimation by distinguishing between multiple MegaTags.
+   *
+   * @param headingDegrees The robot's heading in degrees.
+   * @param headingRateDegrees The robot's rate of rotation in degrees per second.
+   */
   public void setHeadingInfo(double headingDegrees, double headingRateDegrees) {
     visionInterface.setHeadingInfo(headingDegrees, headingRateDegrees);
   }
 
+  /**
+   * Checks if a specified Limelight can see any April Tags.
+   *
+   * @param limelight a limelight (BACK, FRONT_LEFT, FRONT_RIGHT).
+   * @return True if the limelight sees any April Tags, false otherwise.
+   */
   @AutoLogOutput(key = "Vision/Has Targets")
   public boolean canSeeAprilTags(Limelight limelight) {
     return inputs.limelightSeesAprilTags[limelight.getId()];
   }
 
+  /**
+   * Gets the pose of the robot calculated from the April Tags detected by a specified Limelight.
+   *
+   * @param limelight a limelight (BACK, FRONT_LEFT, FRONT_RIGHT).
+   * @return The pose of the robot based on the detected April Tags.
+   */
   public Pose2d getPoseFromAprilTags(Limelight limelight) {
     return inputs.limelightCalculatedPoses[limelight.getId()];
   }
 
+  /**
+   * Gets the last seen pose of the robot based on the latest data from any Limelight.
+   *
+   * @return The last seen pose of the robot.
+   */
   public Pose2d getLastSeenPose() {
     return visionInterface.getLastSeenPose();
   }
