253fc4c0b50c676daf323a28bc5cb9a48d84147e
Ishan1522 <ishanacharyagangopadhyay@gmail.com>
2025-01-17 16:53:16 -0500

    Edited intake a bit

--- src/main/java/frc/robot/commands/intake/Eject.java
+++ src/main/java/frc/robot/commands/intake/Eject.java
@@ -1,19 +1,20 @@
-package frc.robot.commands.drive;
+package frc.robot.commands.intake;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.intake.IntakeConstants;
 import frc.robot.subsystems.intake.IntakeSubsystem;
 
-public class Intake extends Command{
+
+public class Eject extends Command{
   private final IntakeSubsystem intakeSubsystem;
 
-  public Intake(IntakeSubsystem intakeSubsystem){
+  public Eject(IntakeSubsystem intakeSubsystem){
     this.intakeSubsystem = intakeSubsystem;
     addRequirements(this.intakeSubsystem);
   }
 
   public void execute(){
-    intakeSubsystem.setIntakeSpeed(IntakeConstants.intakeSpeed);
+    intakeSubsystem.setIntakeSpeed(IntakeConstants.ejectSpeed);
   }
 
   public void end(){
--- src/main/java/frc/robot/commands/intake/Intake.java
+++ src/main/java/frc/robot/commands/intake/Intake.java
@@ -1,20 +1,19 @@
-package frc.robot.commands.drive;
+package frc.robot.commands.intake;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.intake.IntakeConstants;
 import frc.robot.subsystems.intake.IntakeSubsystem;
 
-
-public class Eject extends Command{
+public class Intake extends Command{
   private final IntakeSubsystem intakeSubsystem;
 
-  public Eject(IntakeSubsystem intakeSubsystem){
+  public Intake(IntakeSubsystem intakeSubsystem){
     this.intakeSubsystem = intakeSubsystem;
     addRequirements(this.intakeSubsystem);
   }
 
   public void execute(){
-    intakeSubsystem.setIntakeSpeed(IntakeConstants.ejectSpeed);
+    intakeSubsystem.setIntakeSpeed(IntakeConstants.intakeSpeed);
   }
 
   public void end(){
