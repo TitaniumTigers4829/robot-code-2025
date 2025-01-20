c13973d9d01abe6fcd399a621b4653e1417e1460
Ishan <ishanacharyagangopadhyay@gmail.com>
2025-01-19 19:41:15 -0500

    delete unused constants

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,7 +9,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.Constants.SimulationConstants;
 import frc.robot.commands.drive.DriveCommand;
 import frc.robot.extras.sim.SimArena;
 import frc.robot.extras.sim.SimGyro;
