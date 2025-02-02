a87d4441df7d38ba652dfa0d17a59379043d32c8
jgelia <jgelia@ncsu.edu>
2025-02-01 19:54:02 -0500

    Updated moving average

--- src/main/java/frc/robot/extras/util/Pose2dMovingAverageFilter.java
+++ src/main/java/frc/robot/extras/util/Pose2dMovingAverageFilter.java
@@ -4,22 +4,46 @@ import edu.wpi.first.math.filter.LinearFilter;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 
+/**
+ * A moving average filter for smoothing Pose2d values.
+ */
 public class Pose2dMovingAverageFilter {
   private final LinearFilter xFilter;
   private final LinearFilter yFilter;
-  private final LinearFilter theataFilter;
+  private final LinearFilter thetaFilter;
 
+  private double filteredX = 0.0;
+  private double filteredY = 0.0;
+  private double filteredTheta = 0.0;
+
+  /**
+   * Constructs a Pose2dMovingAverageFilter with a specified window size.
+   * 
+   * @param windowSize The number of samples to average.
+   */
   public Pose2dMovingAverageFilter(int windowSize) {
     xFilter = LinearFilter.movingAverage(windowSize);
     yFilter = LinearFilter.movingAverage(windowSize);
-    theataFilter = LinearFilter.movingAverage(windowSize);
+    thetaFilter = LinearFilter.movingAverage(windowSize);
   }
 
-  public Pose2d calculate(Pose2d rawPose) {
-    double filteredX = xFilter.calculate(rawPose.getX());
-    double filteredY = yFilter.calculate(rawPose.getY());
-    double filteredTheta = theataFilter.calculate(rawPose.getRotation().getRadians());
+  /**
+   * Adds a new pose to the filter and updates the moving average.
+   * 
+   * @param rawPose The new pose to be added.
+   */
+  public void addPose(Pose2d rawPose) {
+    filteredX = xFilter.calculate(rawPose.getX());
+    filteredY = yFilter.calculate(rawPose.getY());
+    filteredTheta = thetaFilter.calculate(rawPose.getRotation().getRadians());
+  }
 
+  /**
+   * Returns the current averaged pose.
+   * 
+   * @return The averaged Pose2d.
+   */
+  public Pose2d getAverage() {
     return new Pose2d(filteredX, filteredY, new Rotation2d(filteredTheta));
   }
 }
