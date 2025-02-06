392e164a728482a97dd309b9b941a7b3cb7d3da3
Ishan <ishanacharyagangopadhyay@gmail.com>
2025-02-06 11:47:06 -0500

    start

--- src/main/java/frc/robot/extras/util/FrcBody.java
+++ src/main/java/frc/robot/extras/util/FrcBody.java
@@ -16,7 +16,50 @@ import frc.robot.extras.util.geometry.Velocity2d;
 import frc.robot.extras.util.mathutils.GeomUtil;
 import org.dyn4j.dynamics.Body;
 
+/** A Dyn4j Body with additional methods for serialization and conversion to WPILib types. */
 public class FrcBody extends Body {
+
+  /**
+   * A snapshot of the state of a {@link FrcBody}.
+   *
+   * <p>Used for serialization and deserialization.
+   *
+   * <p>Contains all the information needed to recreate a {@link FrcBody} at a later time.
+   *
+   * <p>Contains the following fields:
+   *
+   * <ul>
+   *   <li>{@link Pose2d} pose - the pose of the body. This is the position and rotation of the
+   *       body.
+   *   <li>{@link Mass} mass - the mass of the body. in kilograms.
+   *   <li>{@link MomentOfInertia} momentOfInertia - the moment of inertia of the body. Which is the
+   *       resistance of the body to rotation.
+   *   <li>{@link Velocity2d} velocity - the linear velocity of the body. This is the rate at which
+   *       the body is moving.
+   *   <li>{@link AngularVelocity} angularVelocity - the angular velocity of the body. This is the
+   *       rate at which the body is rotating.
+   *   <li>double linearDamping - the linear damping of the body. This is the rate at which the body
+   *       loses linear velocity.
+   *   <li>double angularDamping - the angular damping of the body. This is the rate at which the
+   *       body loses angular velocity.
+   *   <li>double gravityScale - the gravity scale of the body. This is the factor by which gravity
+   *       affects the body.
+   *   <li>boolean isBullet - whether the body is a bullet. A bullet body is a body that is not
+   *       affected by other bodies during a simulation step.
+   *   <li>double atRestTime - the time the body has been at rest. This is the time the body has not
+   *       been moving.
+   *   <li>{@link Translation2d} forces - the forces acting on the body. This is the force that
+   *       causes the body to move.
+   *   <li>{@link Torque} torque - the torque acting on the body. This is the force that causes the
+   *       body to rotate.
+   *   <li>{@link Translation2d} accumulatedForce - the accumulated forces acting on the body. This
+   *       is the sum of all the forces acting on the body.
+   *   <li>{@link Torque} accumulatedTorque - the accumulated torque acting on the body. This is the
+   *       sum of all the torques acting on the body.
+   * </ul>
+   *
+   * <p>Implements {@link StructSerializable} for serialization and deserialization.
+   */
   public record FrcBodySnapshot(
       Pose2d pose,
       Mass mass,
