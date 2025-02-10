dba189fb2bd3cd2919bc78b10b7bf80da85ef01c
aridavidson001 <aridavidson6@gmail.com>
2025-02-10 09:15:39 -0500

    resets pose at start of auto(uncommented)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.AutoConstants;
@@ -231,13 +232,13 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     // Resets the pose factoring in the robot side
     // This is just a failsafe, pose should be reset at the beginning of auto
-    // swerveDrive.resetEstimatedPose(
-    //     new Pose2d(
-    //         swerveDrive.getEstimatedPose().getX(),
-    //         swerveDrive.getEstimatedPose().getY(),
-    //         Rotation2d.fromDegrees(swerveDrive.getAllianceAngleOffset())));
+    swerveDrive.resetEstimatedPose(
+        new Pose2d(
+            swerveDrive.getEstimatedPose().getX(),
+            swerveDrive.getEstimatedPose().getY(),
+            Rotation2d.fromDegrees(swerveDrive.getAllianceAngleOffset())));
     // if (autoChooser.getSelected() != null) {
-    // return new RunCommand(() -> autoChooser.getSelected());
+    //   return new RunCommand(() -> autoChooser.getSelected());
 
     // } else {
     //   return null;
