5a340ae4b8ccda2e999702e5ed6f9f656fb82a6f
Ishan <ishanacharyagangopadhyay@gmail.com>
2025-02-16 19:10:50 -0500

    decent changes :)

--- src/main/java/frc/robot/subsystems/coralIntake/CoralIntakeConstants.java
+++ src/main/java/frc/robot/subsystems/coralIntake/CoralIntakeConstants.java
@@ -7,9 +7,9 @@ public final class CoralIntakeConstants {
   public static final int EJECT_SPEED = -1;
 
   // the max amount of stator and supply current allowed in the motor, respectively
-  public static final double INTAKE_STATOR_LIMIT = 10000.0;
-  public static final double INTAKE_SUPPLY_LIMIT = 100000.0;
+  public static final double INTAKE_STATOR_LIMIT = 0.0;
+  public static final double INTAKE_SUPPLY_LIMIT = 0.0;
   // Enables stator and supply current limits, respectively
-  public static final boolean INTAKE_STATOR_LIMIT_ENABLE = true;
-  public static final boolean INTAKE_SUPPLY_LIMIT_ENABLE = true;
+  public static final boolean INTAKE_STATOR_LIMIT_ENABLE = false;
+  public static final boolean INTAKE_SUPPLY_LIMIT_ENABLE = false;
 }
