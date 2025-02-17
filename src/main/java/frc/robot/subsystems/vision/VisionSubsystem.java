abc20498280ae8f3dcbcfe4f6a2a78b645c0e323
Ishan1522 <ishanacharyagangopadhyay@gmail.com>
2025-02-17 18:06:35 -0500

    FULLY UD VERY CHEAP

--- src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
@@ -126,21 +126,8 @@ public class VisionSubsystem extends SubsystemBase {
     return visionInterface.isValidMeasurement(limelight);
   }
 
-  /**
-   * @param TARGET_HEIGHT The height of the target in inches
-   * @param TY The vertical angle to the target
-   * @param MA The mount angle
-   */
-  public double getDistanceToTarget(double TARGET_HEIGHT, double TY, double MA) {
-    return (TARGET_HEIGHT / TY + MA); // trig should be right
-  }
 
-  /**
-   * @param TX The
-   * @param TARGET_DISTANCE
-   * @return
-   */
-  public double calculatePoseToTarget(double TX, double TARGET_DISTANCE) {
-    return (TARGET_DISTANCE - TX); // filler this doest work obv
+  public double getDistanceToTargetTrig(Limelight limelight) {
+    return visionInterface.getDistanceToTargetTrig(limelight);
   }
 }
