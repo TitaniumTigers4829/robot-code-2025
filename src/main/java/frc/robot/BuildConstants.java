97af29cf9c080517f1f9b7eda12c52a66955d4bc
jgelia <jgelia@ncsu.edu>
2025-02-19 03:21:15 -0500

    Cleaned up first test case

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-code-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 157;
-  public static final String GIT_SHA = "63824984c981415c2aba2052aa5dee171cf977b7";
-  public static final String GIT_DATE = "2025-02-19 02:22:46 EST";
+  public static final int GIT_REVISION = 158;
+  public static final String GIT_SHA = "ead992002b41559543ce00ecca8ef066e3c70529";
+  public static final String GIT_DATE = "2025-02-19 03:04:08 EST";
   public static final String GIT_BRANCH = "vision-tests";
-  public static final String BUILD_DATE = "2025-02-19 03:03:52 EST";
-  public static final long BUILD_UNIX_TIME = 1739952232577L;
+  public static final String BUILD_DATE = "2025-02-19 03:20:12 EST";
+  public static final long BUILD_UNIX_TIME = 1739953212959L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/test/java/frc/robot/subsystems/vision/PhysicalVisionTest.java
+++ src/test/java/frc/robot/subsystems/vision/PhysicalVisionTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.*;
 
 import edu.wpi.first.networktables.NetworkTable;
@@ -10,11 +11,13 @@ import org.junit.jupiter.api.Test;
 
 public class PhysicalVisionTest {
   private NetworkTableInstance networkTableInstance;
+  private PhysicalVision physicalVision;
 
   @BeforeEach
   void setUp() {
     // Mocks the NetworkTable instance for testing
     networkTableInstance = NetworkTableInstance.getDefault();
+    physicalVision = new PhysicalVision();
   }
 
   @AfterEach
@@ -25,12 +28,25 @@ public class PhysicalVisionTest {
 
   @Test
   void testCanSeeAprilTags() {
+    // We're only going to test one limelight here because all of the logic is the same
     NetworkTable table = networkTableInstance.getTable(Limelight.BACK.getName());
+
+    // Because tv is 0, it means the limelight doesn't see any targets, so it should return false
+    table.getEntry("tv").setDouble(0.0);
+    assertFalse(physicalVision.canSeeAprilTags(Limelight.BACK));
+
+    // Because tx is greater than the accurate FOV, it should return false
     table.getEntry("tv").setDouble(1.0);
+    table.getEntry("tx").setDouble(Limelight.BACK.getAccurateFOV() + 1);
+    assertFalse(physicalVision.canSeeAprilTags(Limelight.BACK));
+
+    // If tv is 1 and tx is less than the accurate FOV, it should return true, regardless
+    // of tx being negative, positive, or zero
+    table.getEntry("tx").setDouble(-(Limelight.BACK.getAccurateFOV() - 1));
+    assertTrue(physicalVision.canSeeAprilTags(Limelight.BACK));
+    table.getEntry("tx").setDouble((Limelight.BACK.getAccurateFOV() - 1));
+    assertTrue(physicalVision.canSeeAprilTags(Limelight.BACK));
     table.getEntry("tx").setDouble(0.0);
-
-    PhysicalVision vision = new PhysicalVision();
-
-    assertTrue(vision.canSeeAprilTags(Limelight.BACK));
+    assertTrue(physicalVision.canSeeAprilTags(Limelight.BACK));
   }
 }
