f525911b3848d5f67bd5d22ceff57061807f1050
Abigail-27 <168038538+Abigail-27@users.noreply.github.com>
2025-02-22 16:42:59 -0500

    climb pivot + algae flywheel (#35)
    * idk what i did
    
    * ermm
    
    * both coral and climb idk how to change branch name purely made by chatgpt plz review it :(
    
    * how u spell algae
    
    * added dealgaefier flywheel subsystem
    
    * i may have just fucked up everything
    
    * huuuuuuuuuuuuuh
    
    * dont look uhh
    
    * yippie??
    
    * what did i dooooo
    
    * help omg
    
    * what
    
    * yuftdy
    
    * dasdfkadsjfjlk
    
    * ghfvh
    
    * flywheel done
    
    * helpppp review the pr so i know what to do pleaseee
    
    Co-authored-by: junocapra <junocapra@users.noreply.github.com>
    
    * i dont think i actually wrote any of this from my own brain
    
    * Formatting fixes
    
    * what am i doinggg
    
    * almost done?????
    
    * push
    
    * I THINK ITS DONE GUYS :OO jkjk look over pr plz
    
    ---------
    
    Co-authored-by: Ishan1522 <ishanacharyagangopadhyay@gmail.com>
    Co-authored-by: aridavidson001 <aridavidson6@gmail.com>
    Co-authored-by: junocapra <junocapra@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-code-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 194;
-  public static final String GIT_SHA = "72e5a7017f4b898cc0611c8042a9a1ac9896f999";
-  public static final String GIT_DATE = "2025-02-21 16:04:44 EST";
-  public static final String GIT_BRANCH = "devbot";
-  public static final String BUILD_DATE = "2025-02-22 11:29:28 EST";
-  public static final long BUILD_UNIX_TIME = 1740241768263L;
+  public static final int GIT_REVISION = 71;
+  public static final String GIT_SHA = "ccc81fce327e5f5b1ce5df44b6b91796d3bcb76e";
+  public static final String GIT_DATE = "2025-02-18 20:25:25 EST";
+  public static final String GIT_BRANCH = "coral-pivot";
+  public static final String BUILD_DATE = "2025-02-22 16:40:54 EST";
+  public static final long BUILD_UNIX_TIME = 1740260454695L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
