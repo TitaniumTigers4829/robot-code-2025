2c1ea1aaf526bf89172c6bf97fd4d9e115cc0151
Ishan <98932677+Ishan1522@users.noreply.github.com>
2025-03-03 11:31:57 -0500

    jiggly wiggly intake thing (#117)
    * haha idk my eyes are asleep
    
    * format
    
    * update variable names
    
    * what da freak broski
    
    * what da freak broski
    
    * fixed
    
    * format

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -36,6 +36,7 @@ import frc.robot.subsystems.climbPivot.SimulatedClimbPivot;
 import frc.robot.subsystems.coralIntake.CoralIntakeConstants;
 import frc.robot.subsystems.coralIntake.CoralIntakeInterface;
 import frc.robot.subsystems.coralIntake.CoralIntakeSubsystem;
+import frc.robot.subsystems.coralIntake.CoralIntakeSubsystem.IntakeState;
 import frc.robot.subsystems.coralIntake.PhysicalCoralIntake;
 import frc.robot.subsystems.coralIntake.SimulatedCoralntake;
 import frc.robot.subsystems.elevator.ElevatorConstants.ElevatorSetpoints;
@@ -96,6 +97,8 @@ public class Robot extends LoggedRobot {
   private AutoChooser autoChooser;
   private Autos autos;
 
+  private boolean isElevatorZeroed;
+
   public Robot() {
     checkGit();
     setupLogging();
@@ -131,6 +134,14 @@ public class Robot extends LoggedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
+
+    // if (isElevatorZeroed == false) {
+    //   Commands.sequence(
+    //       Commands.run(() -> elevatorSubsystem.setVolts(-2.0)).withTimeout(0.5),
+    //       new InstantCommand(() -> elevatorSubsystem.resetPosition(0.0)),
+    //       new InstantCommand(() -> elevatorSubsystem.setVolts(0.0)),
+    //       new InstantCommand(() -> elevatorSubsystem.enableLimits(true, true)));
+    // }
     swerveDrive.resetEstimatedPose(
         new Pose2d(
             swerveDrive.getEstimatedPose().getX(),
@@ -147,6 +158,14 @@ public class Robot extends LoggedRobot {
   /** This function is called once when teleop is enabled. */
   @Override
   public void teleopInit() {
+
+    // if (isElevatorZeroed == false) {
+    //   Commands.sequence(
+    //       Commands.run(() -> elevatorSubsystem.setVolts(-2.0)).withTimeout(0.5),
+    //       new InstantCommand(() -> elevatorSubsystem.resetPosition(0.0)),
+    //       new InstantCommand(() -> elevatorSubsystem.setVolts(0.0)),
+    //       new InstantCommand(() -> elevatorSubsystem.enableLimits(true, true)));
+    // }
     DriverStation.silenceJoystickConnectionWarning(true);
     configureDriverController();
     configureOperatorController();
@@ -260,8 +279,10 @@ public class Robot extends LoggedRobot {
         .leftTrigger()
         .whileTrue(
             Commands.runEnd(
-                () -> coralIntakeSubsystem.setIntakeSpeed(CoralIntakeConstants.EJECT_SPEED),
-                () -> coralIntakeSubsystem.setIntakeSpeed(0.0),
+                () -> coralIntakeSubsystem.setIntakeVelocity(CoralIntakeConstants.EJECT_SPEED),
+                () ->
+                    coralIntakeSubsystem.setIntakeVelocity(
+                        CoralIntakeConstants.NEUTRAL_INTAKE_SPEED),
                 coralIntakeSubsystem));
 
     operatorController
@@ -269,9 +290,10 @@ public class Robot extends LoggedRobot {
         .whileTrue(
             Commands.sequence(
                 elevatorSubsystem.setElevationPosition(ElevatorSetpoints.FEEDER.getPosition()),
+                new InstantCommand(() -> coralIntakeSubsystem.setIntakeState(IntakeState.IDLE)),
                 Commands.runEnd(
-                    () -> coralIntakeSubsystem.intakeCoral(CoralIntakeConstants.INTAKE_SPEED),
-                    () -> coralIntakeSubsystem.setIntakeSpeed(0.0),
+                    () -> coralIntakeSubsystem.intakeCoral(),
+                    () -> coralIntakeSubsystem.setIntakeState(IntakeState.STOPPED),
                     coralIntakeSubsystem)));
 
     operatorController
@@ -280,6 +302,14 @@ public class Robot extends LoggedRobot {
     operatorController
         .povDown()
         .whileTrue(funnelSubsystem.manualFunnel(() -> operatorController.getLeftY()));
+
+    operatorController
+        .povRight()
+        .whileTrue(
+            Commands.runEnd(
+                () -> coralIntakeSubsystem.setIntakeVelocity(2000),
+                () -> coralIntakeSubsystem.setIntakeVelocity(0.0),
+                coralIntakeSubsystem));
 
     // intakeButton.whileTrue(coralIntakeSubsystem.intakeCoral());
     // outakeButton.whileTrue(coralIntakeSubsystem.ejectCoral());
@@ -338,6 +368,11 @@ public class Robot extends LoggedRobot {
 
     // Start AdvantageKit logger
     Logger.start();
+  }
+
+  @Override
+  public void robotInit() {
+    isElevatorZeroed = false;
   }
 
   private void setupSubsystems() {
