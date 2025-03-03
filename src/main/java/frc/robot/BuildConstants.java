b75e72afaee148d668acdcd4477061690911645f
Ishan <ishanacharyagangopadhyay@gmail.com>
2025-03-03 12:21:36 -0500

    fix navx :)

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-code-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 165;
-  public static final String GIT_SHA = "5210b222c8e43e214408794763bafd26d3ac80fb";
-  public static final String GIT_DATE = "2025-02-27 17:29:23 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-01 17:33:59 EST";
-  public static final long BUILD_UNIX_TIME = 1740868439593L;
+  public static final int GIT_REVISION = 174;
+  public static final String GIT_SHA = "9dbf9bddef41bbeaa3a3dcad9ed803561d359492";
+  public static final String GIT_DATE = "2025-03-03 11:29:52 EST";
+  public static final String GIT_BRANCH = "fancy-intake-thingy";
+  public static final String BUILD_DATE = "2025-03-03 12:16:17 EST";
+  public static final long BUILD_UNIX_TIME = 1741022177566L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
