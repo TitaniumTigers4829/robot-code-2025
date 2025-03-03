5a1dd47135484c9fc25a0b32cd98cb76b852a0a9
Ishan <ishanacharyagangopadhyay@gmail.com>
2025-03-03 18:36:06 -0500

    yo idk

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-code-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 165;
-  public static final String GIT_SHA = "5210b222c8e43e214408794763bafd26d3ac80fb";
-  public static final String GIT_DATE = "2025-02-27 17:29:23 EST";
+  public static final int GIT_REVISION = 167;
+  public static final String GIT_SHA = "2c1ea1aaf526bf89172c6bf97fd4d9e115cc0151";
+  public static final String GIT_DATE = "2025-03-03 11:31:57 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-01 17:33:59 EST";
-  public static final long BUILD_UNIX_TIME = 1740868439593L;
+  public static final String BUILD_DATE = "2025-03-03 18:28:45 EST";
+  public static final long BUILD_UNIX_TIME = 1741044525334L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
