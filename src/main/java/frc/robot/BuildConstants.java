94b4f8a76cc96c9d8acd36ce77da9d9e735619cb
aridavidson001 <aridavidson6@gmail.com>
2025-03-12 16:28:12 -0400

    ds

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "robot-code-2025";
+  public static final String MAVEN_NAME = "robot-code-2025-2";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 235;
-  public static final String GIT_SHA = "d73356acb260c8d3b2ebb40f521063ede5efef37";
-  public static final String GIT_DATE = "2025-03-06 17:24:22 EST";
+  public static final int GIT_REVISION = 236;
+  public static final String GIT_SHA = "727621a15bcaf6933d778d1fab38add315ebf526";
+  public static final String GIT_DATE = "2025-03-06 17:41:00 EST";
   public static final String GIT_BRANCH = "Command-Factories";
-  public static final String BUILD_DATE = "2025-03-06 17:38:09 EST";
-  public static final long BUILD_UNIX_TIME = 1741300689539L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-03-12 16:18:48 EDT";
+  public static final long BUILD_UNIX_TIME = 1741810728626L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
