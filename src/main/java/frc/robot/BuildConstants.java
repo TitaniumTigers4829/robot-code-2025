7ef055e109e02107446179b64085ea64593e5743
mychenezpz <mychen@students.chccs.k12.nc.us>
2025-03-29 18:23:34 -0400

    whoops

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "mako-robot-code-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 234;
-  public static final String GIT_SHA = "fd87214208fa2d93cd74015f60ca00a3a06dbff3";
-  public static final String GIT_DATE = "2025-03-29 16:53:43 EDT";
+  public static final int GIT_REVISION = 237;
+  public static final String GIT_SHA = "caaf9304ef06c3d10e84fa71022bda4b8ee07d72";
+  public static final String GIT_DATE = "2025-03-29 18:15:46 EDT";
   public static final String GIT_BRANCH = "maxcrealfunnelstufftheeyesencoder";
-  public static final String BUILD_DATE = "2025-03-29 18:11:06 EDT";
-  public static final long BUILD_UNIX_TIME = 1743286266749L;
+  public static final String BUILD_DATE = "2025-03-29 18:23:00 EDT";
+  public static final long BUILD_UNIX_TIME = 1743286980830L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
