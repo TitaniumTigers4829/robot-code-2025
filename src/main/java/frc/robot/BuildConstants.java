2b6f5f4e39db883d3a5919b4c27e843740db8db7
Ishan <98932677+Ishan1522@users.noreply.github.com>
2025-05-04 15:40:46 -0400

    fuck its all wrong

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-code-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 255;
-  public static final String GIT_SHA = "c3556029b9987e96ed443c20ab5f60b03beae4e5";
-  public static final String GIT_DATE = "2025-05-04 10:59:09 EDT";
+  public static final int GIT_REVISION = 256;
+  public static final String GIT_SHA = "0a45727eca79650b0e124b135425bc8baecf69d7";
+  public static final String GIT_DATE = "2025-05-04 14:40:23 EDT";
   public static final String GIT_BRANCH = "sim-batteries";
-  public static final String BUILD_DATE = "2025-05-04 14:10:44 EDT";
-  public static final long BUILD_UNIX_TIME = 1746382244860L;
+  public static final String BUILD_DATE = "2025-05-04 15:39:08 EDT";
+  public static final long BUILD_UNIX_TIME = 1746387548556L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
