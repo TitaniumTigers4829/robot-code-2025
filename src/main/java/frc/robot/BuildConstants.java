7decc39d54e2f0d670617d435021556a4a581d99
Ishan <98932677+Ishan1522@users.noreply.github.com>
2025-05-06 18:04:55 -0400

    fix gyro sim

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "robot-code-2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 240;
-  public static final String GIT_SHA = "af7c9e5fde4bbf02cee7b0e8054b121612055f36";
-  public static final String GIT_DATE = "2025-04-02 19:08:42 EDT";
-  public static final String GIT_BRANCH = "repulsor-autos";
-  public static final String BUILD_DATE = "2025-04-06 11:07:55 EDT";
-  public static final long BUILD_UNIX_TIME = 1743952075777L;
+  public static final int GIT_REVISION = 247;
+  public static final String GIT_SHA = "6da958df57e66087c232807d8d0e3bf36ae3f328";
+  public static final String GIT_DATE = "2025-05-05 14:47:57 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-05-06 18:01:30 EDT";
+  public static final long BUILD_UNIX_TIME = 1746568890675L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
